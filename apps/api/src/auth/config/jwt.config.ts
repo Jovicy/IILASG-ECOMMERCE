4e4ceda17bed05fa565c4a1b106cb2d1987d248e
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2025-10-20 23:59:02 +0100

    fix: fix typecript check issue

--- apps/api/src/auth/config/jwt.config.ts
+++ apps/api/src/auth/config/jwt.config.ts
@@ -2,8 +2,8 @@ import { registerAs } from '@nestjs/config';
 import { JwtModuleOptions } from '@nestjs/jwt';
 
 export default registerAs('jwt', (): JwtModuleOptions => {
-  const secret = process.env.JWT_SECRET;
-  const expiresInEnv = process.env.JWT_EXPIRES_IN;
+  const secret = process.env.JWT_SECRET as string;
+  const expiresInEnv = process.env.JWT_EXPIRES_IN as string;
   return {
     secret,
     signOptions: {
--- apps/api/src/auth/config/refresh.config.ts
+++ apps/api/src/auth/config/refresh.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { JwtSignOptions } from '@nestjs/jwt';
 
 export default registerAs('refresh-jwt', (): JwtSignOptions => {
-  const expiresInEnv = process.env.REFRESH_JWT_EXPIRES_IN;
+  const expiresInEnv = process.env.REFRESH_JWT_EXPIRES_IN as string;
 
   return {
     secret: process.env.REFRESH_JWT_SECRET || '',
